fd7efa9338fbf1dacacfa7eed39d44cd60017b51
rayjun <rayjun0412@gmail.com>
2018-09-29 16:20:23 +0800

    Init

---
+++ basic/imports.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"math"
+	"fmt"
+)
+
+func main() {
+	fmt.Printf("Now you have %g problems.", math.Sqrt(7))
+}
---
+++ basic/type-inference.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+)
+
+
+
+func main() {
+	v := 42
+	fmt.Printf("v is of type %T\n", v)
+}
