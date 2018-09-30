f9ba66f38a8d59099283389a5f8e0c07576d5935
rayjun <rayjun0412@gmail.com>
2018-09-30 15:50:10 +0800

    Add demo

---
+++ syntax/slice-len-cap.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+)
+
+
+func main() {
+	s := []int{2,3,5,7,11,13}
+	printSlices(s)
+
+	s = s[:0]
+	printSlices(s)
+
+	s = s[:5]
+	printSlices(s)
+
+	s = s[2:]
+	printSlices(s)
+
+	s = s[:5]
+	printSlices(s)
+
+
+}
+
+
+func printSlices(s []int) {
+	fmt.Printf("len=%d cap=%d %v\n", len(s), cap(s),s)
+}
